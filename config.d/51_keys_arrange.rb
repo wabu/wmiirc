58fcf6861b5eb23e5edb2fc7ed66265b1967ef62
Simon Hafner <hafnersimon@gmail.com>
2009-03-06 21:10:59 +0100

    * (keys_arrange) fixed dublicated

--- config.d/51_keys_arrange.rb
+++ config.d/51_keys_arrange.rb
@@ -40,3 +40,16 @@
         curr_view.arrange_in_grid i
       end
     end
+
+  # visual arrangement
+    key Key::ARRANGE + 't' do
+      curr_view.arrange_as_larswm
+    end
+
+    key Key::ARRANGE + 'g' do
+      curr_view.arrange_in_grid
+    end
+
+    key Key::ARRANGE + 'd' do
+      curr_view.arrange_in_diamond
+    end
