96b28fca2340cafdbb1aa1326c8edb117aac2570
wabu <waeber@inf.fu-berlin.de>
2009-05-13 22:45:20 +0200

    changed mixer class to call refresh on self

--- tools/alsa.rb
+++ tools/alsa.rb
@@ -1,16 +1,24 @@
 class Alsa
-  def initialize mixer='Master'
+  def initialize mixer='Master', status_id='volume'
     @mixer = mixer
+    @status_id = status_id
+  end
+
+  def refresh
+    status(@status_id)
   end
 
   def toggle
     system "amixer set #{@mixer} toggle"
+    refresh
   end
   def raise
     system "amixer set #{@mixer} 3dB+"
+    refresh
   end
   def lower
     system "amixer set #{@mixer} 3dB-"
+    refresh
   end
 
   def show_mixer
