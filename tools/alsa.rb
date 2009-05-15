c6163cca4d2c265b7bbc1158218911f9c5bbc5bb
wabu <waeber@inf.fu-berlin.de>
2009-05-15 13:36:08 +0200

    removed refresh form alsa code, as it breaks in upstream status code

--- tools/alsa.rb
+++ tools/alsa.rb
@@ -1,24 +1,17 @@
 class Alsa
-  def initialize mixer='Master', status_id='volume'
+  def initialize mixer='Master'
     @mixer = mixer
     @status_id = status_id
   end
 
-  def refresh
-    status(@status_id)
-  end
-
   def toggle
     system "amixer set #{@mixer} toggle"
-    refresh
   end
   def raise
     system "amixer set #{@mixer} 3dB+"
-    refresh
   end
   def lower
     system "amixer set #{@mixer} 3dB-"
-    refresh
   end
 
   def show_mixer
